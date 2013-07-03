5ce49b7678e380f15c259253461cd61b8292d0fc
CKGrafico <admin@ckgrafico.com>
2013-07-03 15:21:18 +0200

    better structure

--- js/_events.js
+++ js/_events.js
@@ -2,15 +2,7 @@
 var $doc = $(document);
 var $win = $(window);
 
-(function(){
-	$("#left").customSwipe();
-	$("#right").customSwipe({directions : "right"});
-	$("#top").customSwipe({directions : "up"});
-	$("#bottom").customSwipe({directions : "down"});
+// Eventos
+(function() {
 
-	$("#special:not(.off)").customSwipe(function(){
-		$(this).addClass("off");
-	});
-
-	$("#omni").customSwipe({directions : "left,up,right,down"});
 })();
